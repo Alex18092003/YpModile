4bfbc13c06a9af4dc810718b0cfa8f5a377e263b
AleksandraLebedeva <sashyla1809@gmail.com>
2023-03-02 16:32:32 +0300

    Начало

--- app/src/main/java/com/example/meditation/Onboarding.java
+++ app/src/main/java/com/example/meditation/Onboarding.java
@@ -2,7 +2,9 @@ package com.example.meditation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Onboarding extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class Onboarding extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
+    }
+
+    public  void Entrance(View v)
+    {
+        startActivity(new Intent(this, Login.class));
+
     }
 }
