4957ee575f18c6c081eecfc8aba82dfae4a6b241
AleksandraLebedeva <sashyla1809@gmail.com>
2023-03-19 13:10:31 +0300

    RetrofitAPI Добавлен

--- app/src/main/java/com/example/meditation/RetrofitAPI.java
+++ app/src/main/java/com/example/meditation/RetrofitAPI.java
@@ -1,23 +1,19 @@
 package com.example.meditation;
-//import retrofit2.http.Body;
-//import retrofit2.http.DELETE;
-//import retrofit2.http.GET;
-//import retrofit2.http.POST;
-//import retrofit2.Call;
-//import retrofit2.http.PUT;
-//import retrofit2.http.Path;
-//import retrofit2.http.Query;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.Call;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+import retrofit2.http.POST;
 
 public interface RetrofitAPI {
 
-//    Call<DataModal> createPost(@Body DataModal dataModal);
-//
-//    @PUT("Teachers/{id}")
-//    Call<DataModal> updateData(@Path("id") int id, @Body DataModal dataModal);
-//
-//
-//    @DELETE("Teachers/{id}")
-//    Call<DataModal> deleteData(@Path("id") int id);
+    @POST("user/login")
+    Call<MaskUser> createUser (@Body SendUser sendUser);
 
 
 }
