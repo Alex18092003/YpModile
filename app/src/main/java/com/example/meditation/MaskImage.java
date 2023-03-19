563c649d22e9f37a1b066c034e8eac9d512f1c8a
AleksandraLebedeva <sashyla1809@gmail.com>
2023-03-20 00:44:26 +0300

    круглые фото

---
+++ app/src/main/java/com/example/meditation/MaskImage.java
@@ -0,0 +1,6 @@
+package com.example.meditation;
+
+public class MaskImage {
+
+
+}
--- app/src/main/java/com/example/meditation/Profile.java
+++ app/src/main/java/com/example/meditation/Profile.java
@@ -1,14 +1,33 @@
 package com.example.meditation;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
 
 public class Profile extends AppCompatActivity {
 
@@ -21,17 +40,46 @@ public class Profile extends AppCompatActivity {
         ImageView avatar = findViewById(R.id.imageView33);
 
         avatar.setImageBitmap(Main.CurrentUser.getAvatarBitmap());
+
+        Button btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                someActivityResultLauncher.launch(photoPickerIntent);
+            }
+        });
     }
 
 
-    public  void TransitionToMenu(View v)
-    {
+    @Override
+    public void onActivityResult(ActivityResult result) {
+        Bitmap bitmap = null;
+        if (result.getResultCode() == Activity.RESULT_OK) {
+            Uri selectedImage = result.getData().getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public void fhfh() {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        //someActivityResultLauncher.launch(photoPickerIntent);
+    }
+
+
+    public void TransitionToMenu(View v) {
         startActivity(new Intent(this, Menu.class));
 
     }
 
-    public  void TransitionToLogin(View v)
-    {
+    public void TransitionToLogin(View v) {
         SharedPreferences prefs = getSharedPreferences(
                 "Date", Context.MODE_PRIVATE);
         prefs.edit().putString("Avatar", "").apply();
@@ -39,4 +87,6 @@ public class Profile extends AppCompatActivity {
         startActivity(new Intent(this, Login.class));
 
     }
+
+
 }
